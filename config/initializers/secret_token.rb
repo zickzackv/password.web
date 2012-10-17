22ce7c8c5a7bdb20cb62e2447893e3e0722c675f
Paul Spieker <p.spieker@duenos.de>
2012-10-17 20:32:54 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasswordWeb::Application.config.secret_token = 'a910f7f61509ce9ab815641361c8c3b7ae5e36039af29df7205e4ebb5bb59db0727a03f7e95f6b814da0082e30ec71724d6cffeaa08ae5604641dc5a26431d76'
